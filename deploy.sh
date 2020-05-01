baee6b001f9537cae8a27feadb1202d22979aae5
musthaqeem2012 <40406189+musthaqeem2012@users.noreply.github.com>
2020-05-01 14:35:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -29,8 +29,8 @@ if [[ $repo_type == "releases" ]]
    versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/SAMPLE-SNAP/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep -m 1 \ | sed -e 's/\(.*\)<\/value>/\1/' | sed -e 's/ //g')
    #versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/SAMPLE-SNAP/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep 'timestamp')
    echo ${versionTimestamped}
-   versionTimestamped = Regex.Replace(${versionTimestamped}, @"[^0-9]+", "")
-     echo ${versionTimestamped}    
+   versionTimestamped1 = Regex.Replace(${versionTimestamped}, @"[^0-9]+", "")
+     echo ${versionTimestamped1}    
    wget --no-check-certificate "${repo}/repository/SAMPLE-SNAP/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${versionTimestamped}.${type}" -O ${filename}
  fi
 
