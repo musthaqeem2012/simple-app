22af3249710b65bd77a93edbd4d667cd76181ae0
Amjath khan <kh.amjathkhan@gmail.com>
2020-05-01 12:38:13 +0530

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+classifier=$4
+type=$5
+
+if [[ $type == "" ]]; then
+  type="jar"
+fi
+if [[ $classifier != "" ]]; then
+  classifier="-${classifier}"
+fi
+
+groupIdUrl="${groupId//.//}"
+#filename="${artifactId}-${version}${classifier}.${type}"
+filename="${artifactId}-${version}.${type}"
+
+if [[ ${version} == *"SNAPSHOT"* ]]; then repo_type="snapshots"; else repo_type="releases"; fi
+
+if [[ $repo_type == "releases" ]]
+ then
+   wget --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${version}${classifier}.${type}" -O ${filename} -k
+ else
+   versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/SAMPLE-SNAP/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep -m 1 \ | sed -e 's/\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+
+   wget --no-check-certificate "${repo}/repository/SAMPLE-SNAP/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${versionTimestamped}${classifier}.${type}" -O ${filename}
+ fi
