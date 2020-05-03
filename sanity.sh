312e8c35b2c769f42f9efced6059780ce5c8daa8
musthaqeem2012 <40406189+musthaqeem2012@users.noreply.github.com>
2020-05-03 15:30:41 +0530

    Update sanity.sh

--- sanity.sh
+++ sanity.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#if [ -z "$1" ]
-#then
-    #echo "usage: sanity.sh http://www.example.com"
-    #exit 1
-#fi
 
-curl -A 'Mozilla/4.05 [en] (X11; U; Linux 2.0.32 i586)' --location http://www.example.com
+if [ -z "$1" ]
+then
+    echo "usage: sanity.sh http://www.example.com"
+    exit 1
+fi
+
+#curl -A 'Mozilla/4.05 [en] (X11; U; Linux 2.0.32 i586)' --location http://www.example.com
 
 #status=`curl -i --silent --head $1 | head -1 | cut -f 2 -d' '`
 #status = `curl -I -s -L http://www.example.com | grep "HTTP/1.1" | cut -f 2 -d' '`
-status=$(curl -I -s -L http://www.example.com | grep "HTTP/1.1" | cut -f 2 -d' ')
-echo ${status}
 
-#if [$status == "200"]
-#then
-#echo "status was '200'"
-#fi
+status=$(curl -I -s -L $1 | grep "HTTP/1.1" | cut -f 2 -d' ')
+echo "status was : '${status}'"
+ if [ $status -eq 200 ]; then
+	echo "Smoke Test completed successfully"
+ else
+	echo "Smoke Test Failed"
+fi
 
 #echo "status was other than '200': was '$status'"
 #if [ $status != "200 OK" ]
@@ -25,10 +27,10 @@ echo ${status}
 #    exit 1
 #fi
 
-#if [ ! -e "reports" ]
-#then
- # mkdir "reports"
-#fi
+if [ ! -e "reports" ]
+then
+  mkdir "reports"
+fi
 
 #put the xml version of the page into a file
-#curl --silent $1 > reports/smoke.xml
+curl --silent $1 > reports/smoke.xml
